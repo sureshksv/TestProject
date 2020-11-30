3bb60250e251fc33e99e8b9cbf41efe748748525
sureshksv <sureshksv@gmail.com>
2020-11-30 18:28:37 -0500

    withclass27

--- force-app/main/default/lwc/testingGit1/testingGit1.js
+++ force-app/main/default/lwc/testingGit1/testingGit1.js
@@ -1,5 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import getActs from '@salesforce/apex/WiredActsController.getActs'
 
 export default class TestingGit1 extends LightningElement {
     name='SriLakshmi';
+    result;
+    error;
+    
+
+    //function call wire
+    @wire(getActs) 
+    handleApexData({data,error})
+    {
+        if(data)
+        {
+            this.result=data;
+
+        }
+        else
+        {
+            this.error=error;
+        }
+    }
 }
