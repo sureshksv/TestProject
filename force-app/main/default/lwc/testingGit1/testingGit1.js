1f6e9034647ad01867b5e65d3a57b7de2de1269a
sureshksv <sureshksv@gmail.com>
2020-11-30 16:54:41 -0500

    commited 24

--- force-app/main/default/lwc/testingGit1/testingGit1.js
+++ force-app/main/default/lwc/testingGit1/testingGit1.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class TestingGit1 extends LightningElement {}
+export default class TestingGit1 extends LightningElement {
+    name='srilakshmi';
+}
